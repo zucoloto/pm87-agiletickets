0f120a51967167da43c2a10b2ea74f78eaaa5f3c
agil6790 <agil6790@caelumBSB01-06>
2017-06-20 17:29:21 -0300

    removendo testes duplicados

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -6,14 +6,6 @@ import org.junit.Test;
 public class SessaoTest {
 
 	@Test
-	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(2);
-
-		Assert.assertTrue(sessao.podeReservar(1));
-	}
-
-	@Test
 	public void deveVender5ingressosSeHa10vagas() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(10);
